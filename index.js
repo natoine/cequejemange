210ab023711d850f1a70e9e65b78b5b6648bd138
natoine <antoineseilles@gmail.com>
2020-12-06 19:49:34 +0100

    databse connection base on otakuotake actual code

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 'use strict'
 
-var express = require('express');
-var app = express();
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
 
+const port = process.env.PORT || 3000 ;
+var nbvisit = 0 ;
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+const mongodb = require('mongodb');
+
+const urimongo = require("./resources/secret/databaseconfig.js").url ;
+console.log("uri mongodb", urimongo);
+
+//serves static files
+app.use(express.static('resources/public'));
+
+app.get("/whatismyname", function(request, response) {
+    mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }, function (err, client) {
+        if(err) console.log("error" , err);
+        else {
+            console.log("connected !");
+            client.db("otakuotake").collection("supersayans").find().toArray(function(err, items) {
+                if(err) throw err;     
+                response.send(items[0].name);          
+            });
+        }
+    });
 });
 
-
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
+    console.log('Serveur listening on port ' + port);
 });
